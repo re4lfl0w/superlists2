8c84710a645f4fec9717636b87998a8bd0bd5724
re4lfl0w <re4lfl0w@gmail.com>
2016-04-17 21:21:43 +0900

    단위 테스트를 2개 파일로 나누기

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -20,33 +20,6 @@ class HomePageTest(TestCase):
         self.assertEqual(response.content.decode(), expected_html)
 
 
-class ListAndItemModelsTest(TestCase):
-    def test_saving_and_retrieving_items(self):
-        '''아이템을 저장하고 다시 가져왔을 때 정상적인지?'''
-        list_ = List()
-        list_.save()
-
-        first_item = Item()
-        first_item.text = '첫 번째 아이템'
-        first_item.list = list_
-        first_item.save()
-
-        second_item = Item()
-        second_item.text = '두 번째 아이템'
-        second_item.list = list_
-        second_item.save()
-
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-        self.assertEqual(first_saved_item.text, '첫 번째 아이템')
-        self.assertEqual(first_saved_item.list, list_)
-        self.assertEqual(second_saved_item.text, '두 번째 아이템')
-        self.assertEqual(second_saved_item.list, list_)
-
-
 class ListViewTest(TestCase):
     def test_uses_list_template(self):
         '''특정한 뷰에서 내가 지정한 템플릿 파일이 사용되는지?'''
