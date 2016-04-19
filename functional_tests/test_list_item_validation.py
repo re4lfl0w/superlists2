c548715acb942393ca4374361e104939c6d87e27
re4lfl0w <re4lfl0w@gmail.com>
2016-04-19 21:44:53 +0900

    리스트 뷰에서 모델 유효성 검증 구현

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from time import sleep
 from unittest import skip
 from .base import FunctionalTest
 
@@ -26,6 +27,8 @@ class ItemValidationTest(FunctionalTest):
         self.check_for_row_in_list_table('1: 우유 사기')
         error = self.browser.find_element_by_css_selector('.has-error')
         self.assertEqual(error.text, "You can't have an empty list item")
+
+        sleep(1)
 
         # 아이템을 입력하면 정상 동작한다
         self.browser.find_element_by_id('id_new_item').send_keys('tea 만들기\n')
